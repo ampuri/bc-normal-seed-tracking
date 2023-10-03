0998dbf1796441fbe9b39840541bf61f23e682f4
ampuri <crashfeverxd@gmail.com>
2023-10-02 22:48:23 -0400

    Add some info

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RollTable from "./RollTable";
 import styled from "@emotion/styled";
+import { Typography } from "@mui/material";
 
 const Styles = styled.div`
   * {
@@ -16,6 +17,32 @@ const Styles = styled.div`
 const Page = () => {
   return (
     <Styles>
+      <Typography variant="h4">BC Normal Seed Tracker</Typography>
+      <Typography variant="h6">About this tool:</Typography>
+      <Typography variant="body1">
+        <ul>
+          <li>
+            The{" "}
+            <strong style={{ fontWeight: "bold" }}>
+              normal gacha does NOT share a seed with the rare gacha.
+            </strong>{" "}
+            You'll need to find your seed separately.
+          </li>
+          <li>
+            To input a seed, change the URL to have a parameter like ?seed=123
+          </li>
+          <li>
+            Track switches work the exact same as rare banners. You can
+            strategically roll to hit or avoid a track switches.
+          </li>
+          <li>
+            Clicking an entry will update your seed, but clicking track switches
+            doesn't work. You'll need to find the destination manually and
+            update.
+          </li>
+        </ul>
+      </Typography>
+      <Typography variant="body1">&nbsp;</Typography>
       <RollTable />
     </Styles>
   );
